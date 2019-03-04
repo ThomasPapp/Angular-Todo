db607fe2b0cbe6e95bcd7ff3c85498215cc089cc
Thomas Papp <pappmthomas@gmail.com>
2019-03-03 21:14:03 -0600

    Added a basic todo list. Supports adding new tasks, removing, and clearing tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'todo-app';
+export class AppComponent implements OnInit {
+  title = 'my first Angular project!';
+
+  taskForm: FormGroup;
+
+  tasks: string[] = [];
+
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  ngOnInit() {
+    this.taskForm = this.fb.group({
+      task: ''
+    });
+  }
+
+  addTask() {
+    const {task} = this.taskForm.value;
+    this.taskForm.reset();
+    this.tasks.push(task);
+  }
+
+  removeTask(index) {
+    this.tasks.splice(index, 1);
+  }
+
+  clearTasks() {
+    this.tasks = [];
+  }
 }
